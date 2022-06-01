ca115468a1ae95e97f9c68ed74b8d2b8f112df65
ThiGouvea <thiagouvea90@gmail.com>
2022-06-01 17:23:57 -0300

    adcionado movimentação e imagens ao jogo

--- fighter.py
+++ fighter.py
@@ -3,10 +3,15 @@ import pygame
 
 class Fighter():
     def __init__(self, x, y):
+        self.flip = False
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        self.health = 100
 
-    def move(self, screen_width):
+    def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
@@ -15,14 +20,25 @@ class Fighter():
         # aceita o pressionamento de teclas
         key = pygame.key.get_pressed()
 
-        # movimento
-        if key[pygame.K_a]:
-            dx = -SPEED
-        if key[pygame.K_d]:
-            dx = SPEED
-        # pulo
-        if key[pygame.K_w]:
-            self.vel_y = -30
+        # só se pode fazer outras ações se não estiver atacando no momento
+        if self.attacking ==False:
+            # movimento
+            if key[pygame.K_a]:
+                dx = -SPEED
+            if key[pygame.K_d]:
+                dx = SPEED
+            # pulo
+            if key[pygame.K_w] and self.jump == False:
+                self.vel_y = -30
+                self.jump = True
+            # ataque
+            if key[pygame.K_r] or key[pygame.K_t]:
+                self.attack(surface, target)
+                # determina qual tipo de ataque foi usado
+                if key[pygame.K_r]:
+                    self.attack_type = 1
+                if key[pygame.K_t]:
+                    self.attack_type = 2
 
         # aplica a força da gravidade
         self.vel_y += GRAVITY
@@ -33,11 +49,29 @@ class Fighter():
             dx = -self.rect.left
         if self.rect.right + dx > screen_width:
             dx = screen_width - self.rect.right
-        if self.rect.bottom
+        if self.rect.bottom + dy > screen_height - 110:
+            self.vel_y = 0
+            self.jump = False
+            dy = screen_height - 110 - self.rect.bottom
+
+        # certifica que os jogadores estão de frente um pro outro
+        if target.rect.centerx > self.rect.centerx:
+            self.flip = False
+        else:
+            self.flip = True
 
         # atualiza a posição do jogador na tela
         self.rect.x += dx
         self.rect.y += dy
+
+
+    def attack(self, surface, target):
+        self.attacking = True
+        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height)
+        if attacking_rect.colliderect(target.rect):
+            target.health -= 10
+
+        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
--- main.py
+++ main.py
@@ -14,6 +14,11 @@ pygame.display.set_caption("Brawler")
 clock = pygame.time.Clock()
 FPS = 60
 
+# define as cores
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
 # carregar uma imagem de fundo
 bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
 
@@ -21,6 +26,13 @@ bg_image = pygame.image.load("assets/images/background/background.jpg").convert_
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0, 0))
+
+    # função que desenha o HP dos lutadores na tela
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 
 # cria duas instancias de lutadores
@@ -36,8 +48,12 @@ while run:
     # desenhar o fundo
     draw_bg()
 
+    # mostra os status do lutador
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 580, 20)
+
     # movimenta lutadores
-    fighter_1.move(SCREEN_WIDTH)
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
 
     # desenha os lutadores na tela
     fighter_1.draw(screen)
